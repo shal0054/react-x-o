d12a33a4f6a0144ded4f31b6a52e37fe686d15f0
Karim Shaloh <karimshaloh@Karims-MBP.home>
2023-07-13 23:15:51 -0400

    updated readme

---
+++ src/App.js
@@ -0,0 +1,19 @@
+export default function Square() {
+	return (
+		<div>
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+			<br />
+			<br />
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+			<br />
+			<br />
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+		</div>
+	);
+}
